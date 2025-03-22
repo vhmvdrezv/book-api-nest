7530a0786e1be9d7d08dee1cc52fccd86767f77d
vhmvdrezv <ahmdrzashn@gmail.com>
2025-03-22 22:30:40 +0330

    get writer by id

--- src/writers/writers.controller.ts
+++ src/writers/writers.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { WritersService } from './writers.service';
 import { CreateWriterDto } from './dto/create-writer.dto';
 
@@ -9,6 +9,11 @@ export class WritersController {
     @Get()
     async getAllWriters() {
         return this.writersService.getAllWriters();
+    }
+
+    @Get(':id')
+    async getWriterById(@Param('id', ParseIntPipe) id: number) {
+        return this.writersService.getWriterById(id);
     }
 
     @Post()
--- src/writers/writers.service.ts
+++ src/writers/writers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { CreateWriterDto } from './dto/create-writer.dto';
 
@@ -7,7 +7,13 @@ export class WritersService {
     constructor(private readonly databaseService: DatabaseService) { };
 
     async getAllWriters() {
-        const writers = await this.databaseService.writer.findMany();
+        const writers = await this.databaseService.writer.findMany({
+            select: {
+                id: true,
+                name: true,
+                bio: true
+            }
+        });
         return {
             status: 'success',
             message: 'list of writers',
@@ -29,6 +35,23 @@ export class WritersService {
             status: 'success',
             message: 'writer created successfully.',
             data: newWriter
+        };
+    }
+
+    async getWriterById(id: number) {
+        const writer = await this.databaseService.writer.findUnique({
+            where: { id },
+            select: {
+                id: true,
+                name: true,
+                bio: true
+            }
+        });
+        if (!writer) throw new NotFoundException(`writer with id ${id} not found.`);
+        return {
+            status:'success',
+            message: 'writer: ',
+            data: writer
         };
     }
 }
